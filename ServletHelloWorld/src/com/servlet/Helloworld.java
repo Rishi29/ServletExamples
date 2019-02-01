3316ac5af575e360ea4a8452fff5bb30b679bed8
Rishikesh Mandal <me.rishi29@gmail.com>
2019-02-01 17:01:36 -0500

    Hello World in Servlet Example. First Commit.

---
+++ ServletHelloWorld/src/com/servlet/Helloworld.java
@@ -0,0 +1,34 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet("/Helloworld")
+public class Helloworld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private String message;
+
+	public void init() throws ServletException {
+
+		message = "Hello World";
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + message + "</h1>");
+	}
+
+}
